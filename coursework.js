51affcdb9c9035ec50846789614c6011de9898fe
Iman Usman <imanusman5053@yahoo.com>
2022-01-27 19:05:11 +0300

    fixed path lessons

---
+++ coursework.js
@@ -0,0 +1,17 @@
+const express = require("express"); 
+const http = require('http');
+const app = express(); 
+app.get ("lessons", function(request, response) { 
+    response.send(lessons =[
+        {'topic': 'math','location':'london','price': 100},
+        {'topic': 'math','location':'Colindale','price': 80},
+        {'topic': 'math','price': 90},
+        {'topic': 'math','location':'bristol','price': 120},
+    ]);
+});
+
+app.get ("users", function(request, response) { 
+    response.send({'email': 'user@email.com', 'password':'mypassword'});
+});
+
+http.createServer(app).listen(300); 
