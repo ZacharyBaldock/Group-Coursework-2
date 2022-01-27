26e57f9cfcccd2e5feda0697abb68cd26ec1cc27
Iman Usman <imanusman5053@yahoo.com>
2022-01-27 20:01:12 +0300

    Updated

--- coursework.js
+++ coursework.js
@@ -1,17 +1,23 @@
 const express = require("express"); 
 const http = require('http');
 const app = express(); 
-app.get ("/lessons", function(request, response) { 
-    response.send(lessons =[
-        {'topic': 'math','location':'hendon','price': 100},
-        {'topic': 'math','location':'colindale','price': 80},
-        {'topic': 'math','location':'brent cross','price': 90},
-        {'topic': 'math','location':'golders green','price': 120},
-    ]);
-});
+const port=3000
+const courses =[
+    {'topic': 'math','location':'hendon','price': 100,},
+    {'topic': 'math','location':'Colindale','price': 80},
+    {'topic': 'math','location':'brent cross','price': 90},
+    {'topic': 'math','location':'golders green','price': 120},
+];
 
-app.get ("/users", function(request, response) { 
-    response.send({'email': 'user@email.com', 'password':'mypassword'});
-});
+app.get("/users",(req, res) =>{
+    res.status(200).json({'email':'user@gmail.com', 'password':'mypassword'})
+})
+app.get("/lessons",(req, res) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end(JSON.stringify(courses));
+})
+.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+  })
 
-http.createServer(app).listen(300); 
